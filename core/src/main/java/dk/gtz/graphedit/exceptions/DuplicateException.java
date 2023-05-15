dc440160274191157fe33ff8a8b3a55a9632da48
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-05-15 06:43:07 +0200

    feat: add some custom exceptions

---
+++ core/src/main/java/dk/gtz/graphedit/exceptions/DuplicateException.java
@@ -0,0 +1,8 @@
+package dk.gtz.graphedit.exceptions;
+
+public class DuplicateException extends Exception {
+    public DuplicateException(String message) {
+        super(message);
+    }
+}
+
---
+++ core/src/main/java/dk/gtz/graphedit/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package dk.gtz.graphedit.exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
+
