9e012178419a9efa97cd9bf4f10cd2735937299f
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-06-11 22:53:53 +0200

    feat: add arrow ends to the edges
    This should be generalized and be customizable... But this is good enough for now

--- core/src/main/java/dk/gtz/graphedit/view/EdgeController.java
+++ core/src/main/java/dk/gtz/graphedit/view/EdgeController.java
@@ -2,13 +2,20 @@ package dk.gtz.graphedit.view;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import dk.gtz.graphedit.viewmodel.ViewModelEdge;
 import dk.gtz.graphedit.viewmodel.ViewModelProjectResource;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
 import javafx.scene.Group;
-import javafx.scene.paint.Color;
+import javafx.scene.Node;
 import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
 
 public class EdgeController extends Group {
+    private static Logger logger = LoggerFactory.getLogger(EdgeController.class);
     private final UUID edgeKey;
     private final ViewModelEdge edgeValue;
 
@@ -19,13 +26,13 @@ public class EdgeController extends Group {
     }
 
     private void initialize(ViewModelProjectResource resource) {
-	getChildren().add(initializeLinePresentation(resource));
+	var line = initializeLinePresentation(resource);
+	getChildren().addAll(line, initializeLeftArrow(line), initializeRightArrow(line));
     }
 
     private Line initializeLinePresentation(ViewModelProjectResource resource) {
 	var edgePresentation = new Line();
-	edgePresentation.setStroke(Color.WHITE);
-	// subscribe on events
+	edgePresentation.getStyleClass().add("stroke-primary");
 	var sourceVertex = resource.syntax().vertices().getValue().get(edgeValue.source().getValue());
 	var targetVertex = resource.syntax().vertices().getValue().get(edgeValue.target().getValue());
 	edgePresentation.startXProperty().bind(sourceVertex.position().getXProperty());
@@ -35,5 +42,38 @@ public class EdgeController extends Group {
 	return edgePresentation;
     }
 
+    private Node initializeLeftArrow(Line line) {
+	var arrowLength = 20;
+        var leftArrow = new Line();
+	leftArrow.getStyleClass().add("stroke-primary");
+        leftArrow.startXProperty().bind(line.endXProperty());
+        leftArrow.startYProperty().bind(line.endYProperty());
+        leftArrow.endXProperty().bind(line.endXProperty().subtract(arrowLength * 1.5));
+        leftArrow.endYProperty().bind(line.endYProperty().subtract(arrowLength / 2));
+	rotateWithLine(line, leftArrow, leftArrow.startXProperty(), leftArrow.startYProperty());
+        return leftArrow;
+    }
+
+    private Node initializeRightArrow(Line line) {
+	var arrlowLength = 20;
+        var rightArrow = new Line();
+	rightArrow.getStyleClass().add("stroke-primary");
+        rightArrow.startXProperty().bind(line.endXProperty());
+        rightArrow.startYProperty().bind(line.endYProperty());
+        rightArrow.endXProperty().bind(line.endXProperty().subtract(arrlowLength * 1.5));
+        rightArrow.endYProperty().bind(line.endYProperty().add(arrlowLength / 2));
+	rotateWithLine(line, rightArrow, rightArrow.startXProperty(), rightArrow.startYProperty());
+        return rightArrow;
+    }
+
+    private void rotateWithLine(Line line, Node nodeToTransform, DoubleProperty pivotX, DoubleProperty pivotY) {
+	var rotate = new Rotate();
+	rotate.pivotXProperty().bind(pivotX);
+	rotate.pivotYProperty().bind(pivotY);
+	rotate.angleProperty().bind(Bindings.createDoubleBinding(() -> 
+		    (Math.atan2(line.getEndY() - line.getStartY(), line.getEndX() - line.getStartX()) * 180 / Math.PI),
+		    line.endYProperty(), line.endXProperty(), line.startXProperty(), line.startYProperty()));
+	nodeToTransform.getTransforms().add(rotate);
+    }
 }
 
--- core/src/main/java/dk/gtz/graphedit/viewmodel/ViewModelVertex.java
+++ core/src/main/java/dk/gtz/graphedit/viewmodel/ViewModelVertex.java
@@ -2,6 +2,7 @@ package dk.gtz.graphedit.viewmodel;
 
 import dk.gtz.graphedit.model.ModelVertex;
 
+// TODO: Should also keep some information about the shape (width, height, shape (oval,rectangle)) for presentation purposes
 public record ViewModelVertex(ViewModelPoint position) {
     public ViewModelVertex(ModelVertex vertex) {
         this(new ViewModelPoint(vertex.position()));
