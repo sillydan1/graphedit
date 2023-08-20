370618a1132eab35fc167fa491b23cdc5ff8e33c
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-08-20 15:56:05 +0200

    fix: todos in EditorLogAppender

--- core/src/main/java/dk/gtz/graphedit/logging/EditorLogAppender.java
+++ core/src/main/java/dk/gtz/graphedit/logging/EditorLogAppender.java
@@ -1,7 +1,8 @@
 package dk.gtz.graphedit.logging;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 import ch.qos.logback.classic.Level;
@@ -11,22 +12,38 @@ import javafx.application.Platform;
 
 public class EditorLogAppender extends AppenderBase<ILoggingEvent> {
     private static record LogConsumer(Level level, Consumer<String> consumer) {}
-    private static List<LogConsumer> subscribers = new ArrayList<>();
+    private static Map<UUID, LogConsumer> subscribers = new HashMap<>();
 
     @Override
     protected void append(ILoggingEvent eventObject) { }
 
     @Override
     public synchronized void doAppend(ILoggingEvent eventObject) {
-	var messageCopy = eventObject.getMessage(); // TODO: This is not actually formatted yet. So strings will look like so: "hello '{}'" instead of "hello 'foo'"
-	subscribers.stream()
-	    .filter(e -> e.level() == eventObject.getLevel())
-	    .forEach(r -> Platform.runLater(() -> r.consumer().accept(messageCopy)));
+	var messageCopy = eventObject.getFormattedMessage();
+	subscribers.entrySet().stream()
+	    .filter(e -> e.getValue().level() == eventObject.getLevel())
+	    .forEach(r -> Platform.runLater(() -> r.getValue().consumer().accept(messageCopy)));
     }
 
-    // TODO: How to unsubscribe again?
-    public static void subscribe(Level levelFilter, Consumer<String> logConsumer) {
-	subscribers.add(new LogConsumer(levelFilter, logConsumer));
+    /**
+     * Add a logconsume function to the list of consumers
+     * @param levelFilter the log level at which the logconsumer function will be invoked
+     * @param logConsumer the logconsumer function to invoke
+     * @return the registered key. Use this to unsubscribe again later if needed
+     */
+    public static UUID subscribe(Level levelFilter, Consumer<String> logConsumer) {
+	var key = UUID.randomUUID();
+	subscribers.put(key, new LogConsumer(levelFilter, logConsumer));
+	return key;
+    }
+
+    /**
+     * Will remove the logconsume function with the associated key.
+     * @param key the identifying key of the logconsumer to remove
+     */
+    public static void unsubscribe(UUID key) {
+	if(subscribers.containsKey(key))
+	    subscribers.remove(key);
     }
 }
 
