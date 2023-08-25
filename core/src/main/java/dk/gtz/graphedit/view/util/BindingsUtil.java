b989cfff309d98da24d5c849eff6ebad13cc0c9f
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-08-25 20:35:56 +0200

    fix: extract shapeutil into its own class

--- core/src/main/java/dk/gtz/graphedit/view/util/BindingsUtil.java
+++ core/src/main/java/dk/gtz/graphedit/view/util/BindingsUtil.java
@@ -10,37 +10,6 @@ import javafx.scene.shape.Line;
 import javafx.scene.transform.Affine;
 
 public class BindingsUtil {
-    // TODO: This is a MathUtil or ShapeUtil func. Not a BindingsUtil
-    /**
-     * Will calculate the intersection point on a reactangle shape given a ray (dx,dy)
-     * @see <a href="https://math.stackexchange.com/questions/2738250/intersection-of-ray-starting-inside-square-with-that-square/2738727#2738727">inspired stackoverflow answer</a>
-     * @param rect rectangle dimensions
-     * @param dx ray x direction
-     * @param dy ray y direction
-     * @throws RuntimeException if the provided shape is not marked as a rectangle
-     * @return The intersection point on the rectangle
-     * @implNote Will return (0,0) if the both dx and dy are zero
-     */
-    private static ViewModelPoint rectangleIntersect(ViewModelVertexShape rect, double dx, double dy) {
-	if(!rect.shapeType().get().equals(ViewModelShapeType.RECTANGLE))
-	    throw new RuntimeException("Shape is not a rectangle");
-	if(dx == 0 && dy == 0)
-	    return new ViewModelPoint(0, 0);
-	var halfWidth = (rect.widthProperty().get() * rect.scaleXProperty().get()) / 2;
-	var halfHeight = (rect.heightProperty().get() * rect.scaleYProperty().get()) / 2;
-	if(dx < 0)
-	    halfWidth = -halfWidth;
-	if(dy < 0)
-	    halfHeight = -halfHeight;
-	var tx = halfWidth / dx;
-	var ty = halfHeight / dy;
-	if(tx < ty)
-	    return new ViewModelPoint(tx * dx, tx * dy);
-	if(tx > ty)
-	    return new ViewModelPoint(ty * dx, ty * dy);
-	return new ViewModelPoint(tx * dx, ty * dy);
-    }
-
     public static DoubleBinding createShapedXBinding(ViewModelPoint sourcePosition, ViewModelPoint targetPosition, ViewModelVertexShape shape) {
 	if(shape.shapeType().get().equals(ViewModelShapeType.RECTANGLE))
 	    return createRectangularXBinding(sourcePosition, targetPosition, shape);
@@ -53,7 +22,7 @@ public class BindingsUtil {
 	return Bindings.createDoubleBinding(() -> {
 	    var diffX = targetPosition.getXProperty().get() - sourcePosition.getXProperty().get();
 	    var diffY = targetPosition.getYProperty().get() - sourcePosition.getYProperty().get();
-	    var intersection = rectangleIntersect(shape, diffX, diffY);
+	    var intersection = ShapeUtil.rectangleIntersect(shape, diffX, diffY);
 	    return targetPosition.getXProperty().get() - intersection.getX();
 	},
 	sourcePosition.getXProperty(), sourcePosition.getYProperty(),
@@ -87,7 +56,7 @@ public class BindingsUtil {
 	return Bindings.createDoubleBinding(() -> {
 	    var diffX = targetPosition.getXProperty().get() - sourcePosition.getXProperty().get();
 	    var diffY = targetPosition.getYProperty().get() - sourcePosition.getYProperty().get();
-	    var intersection = rectangleIntersect(shape, diffX, diffY);
+	    var intersection = ShapeUtil.rectangleIntersect(shape, diffX, diffY);
 	    return targetPosition.getYProperty().get() - intersection.getY();
 	},
 	sourcePosition.getXProperty(), sourcePosition.getYProperty(),
