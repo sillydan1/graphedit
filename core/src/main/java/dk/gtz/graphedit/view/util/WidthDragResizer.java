e9d10a200c63c0d396e9623c5cf968cfb4580cdb
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-09-02 09:29:08 +0200

    fix: add tooltip to unified modelling tool
    feat: resizeable file and log panes

---
+++ core/src/main/java/dk/gtz/graphedit/view/util/WidthDragResizer.java
@@ -0,0 +1,86 @@
+package dk.gtz.graphedit.view.util;
+
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+
+public class WidthDragResizer {
+    /**
+     * The margin around the control that a user can click in to start resizing
+     * the region.
+     */
+    private static final int RESIZE_MARGIN = 5;
+    private final Region region;
+    private double scalar, startX, startWidth;
+    private boolean initMinWidth;
+    private boolean dragging;
+    private boolean dragFromLeft;
+
+    private WidthDragResizer(Region region, boolean inverted) {
+        this.region = region;
+        this.initMinWidth = false;
+        this.scalar = inverted ? -1 : 1;
+        this.dragFromLeft = inverted;
+    }
+
+    private static void makeResizable(Region region, boolean inverted) {
+        var resizer = new WidthDragResizer(region, inverted);
+        region.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> resizer.mousePressed(e));
+        region.addEventFilter(MouseEvent.MOUSE_DRAGGED, e -> resizer.mouseDragged(e));
+        region.addEventFilter(MouseEvent.MOUSE_MOVED, e -> resizer.mouseOver(e));
+        region.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> resizer.mouseReleased(e));
+    }
+
+    public static void makeResizableRight(Region region) {
+        makeResizable(region, false);
+    }
+
+    public static void makeResizableLeft(Region region) {
+        makeResizable(region, true);
+    }
+
+    private void mouseReleased(MouseEvent event) {
+        dragging = false;
+        region.setCursor(Cursor.DEFAULT);
+        startX = 0;
+        startWidth = 0;
+    }
+
+    private void mouseOver(MouseEvent event) {
+        if(isInDraggableZone(event) || dragging)
+            region.setCursor(Cursor.H_RESIZE);
+        else
+            region.setCursor(Cursor.DEFAULT);
+    }
+
+    private boolean isInDraggableZone(MouseEvent event) {
+        if(dragFromLeft)
+            return event.getX() < RESIZE_MARGIN;
+        return event.getX() > (region.getWidth() - RESIZE_MARGIN);
+    }
+
+    private void mouseDragged(MouseEvent event) {
+        if(!dragging)
+            return;
+        var mousex = event.getX();
+        var dx = (mousex - startX) * scalar;
+        var newWidth = startWidth + dx;
+        region.setPrefWidth(newWidth);
+    }
+
+    private void mousePressed(MouseEvent event) {
+        if(!isInDraggableZone(event))
+            return;
+        startX = event.getX();
+        startWidth = region.getWidth();
+        dragging = true;
+        // make sure that the minimum Width is set to the current Width once,
+        // setting a min Width that is smaller than the current Width will
+        // have no effect
+        if (!initMinWidth) {
+            region.setMinWidth(region.getWidth());
+            initMinWidth = true;
+        }
+    }
+}
+
