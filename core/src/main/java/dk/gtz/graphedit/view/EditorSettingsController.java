2747a2c2491e9e611f6f9f96d290c241ef5e73b4
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-09-17 10:24:55 +0200

    fix: editor settings styling

--- core/src/main/java/dk/gtz/graphedit/view/EditorSettingsController.java
+++ core/src/main/java/dk/gtz/graphedit/view/EditorSettingsController.java
@@ -3,50 +3,51 @@ package dk.gtz.graphedit.view;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import atlantafx.base.theme.Styles;
+import atlantafx.base.controls.Tile;
+import atlantafx.base.controls.ToggleSwitch;
 import dk.gtz.graphedit.tool.EditorActions;
 import dk.gtz.graphedit.view.util.InspectorUtils;
 import dk.gtz.graphedit.viewmodel.ViewModelEditorSettings;
 import dk.yalibs.yadi.DI;
 import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class EditorSettingsController {
     private static final Logger logger = LoggerFactory.getLogger(EditorSettingsController.class);
     @FXML
-    private BorderPane root;
-    @FXML
     private VBox inspectorPane;
     private ViewModelEditorSettings editorSettings;
 
     public void initialize() {
-	initRoot();
 	editorSettings = DI.get(ViewModelEditorSettings.class);
-	addInspector("gridsizeX", editorSettings.gridSizeX());
-	addInspector("gridsizeY", editorSettings.gridSizeY());
-	addInspector("gridsnap", editorSettings.gridSnap());
-	addInspector("useLightTheme", editorSettings.useLightTheme());
-	addInspector("autoOpenLastProject", editorSettings.autoOpenLastProject());
+	addInspector("Use GridSnap", "Makes vertices snap to the background grid", editorSettings.gridSnap());
+	addInspector("GridSnap XSize", "", editorSettings.gridSizeX());
+	addInspector("GridSnap YSize", "", editorSettings.gridSizeY());
+	addInspector("Light Theme", "Use light theme", editorSettings.useLightTheme());
+	addInspector("Auto Open", "Automatically open the project you closed last", editorSettings.autoOpenLastProject());
 	addSaveButton();
     }
 
-    private void initRoot() {
-	root.getStyleClass().add(Styles.BG_DEFAULT);
-    }
-
-    private void addInspector(String labelName, Observable observable) {
-	inspectorPane.getChildren().add(new HBox(new Label(labelName), InspectorUtils.getObservableInspector(observable)));
+    private void addInspector(String labelName, String description, Observable observable) {
+	var inspector = InspectorUtils.getObservableInspector(observable);
+	var tile = new Tile(labelName, description);
+	tile.setAction(inspector);
+	if(inspector instanceof ToggleSwitch ts)
+	    tile.setActionHandler(ts::fire);
+	inspectorPane.getChildren().add(tile);
     }
 
     private void addSaveButton() {
 	var saveButton = new Button("Save Changes");
 	saveButton.setOnAction((e) -> EditorActions.saveEditorSettings(editorSettings));
-	inspectorPane.getChildren().add(saveButton);
+	var pane = new HBox(saveButton);
+	pane.setAlignment(Pos.CENTER);
+	inspectorPane.getChildren().addAll(new Separator(), pane);
     }
 }
 
