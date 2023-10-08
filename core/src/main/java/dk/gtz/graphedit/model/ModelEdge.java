159bebc3e5c381892b88742272698f4c845309ad
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-10-08 20:15:41 +0200

    fix: model edges are classes now too
    this is more future proof in terms of serialization

--- core/src/main/java/dk/gtz/graphedit/model/ModelEdge.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelEdge.java
@@ -5,5 +5,25 @@ import java.util.UUID;
 /**
  * An edge from some source to some target. May connect any syntactic element
  */
-public record ModelEdge(UUID source, UUID target) {}
+public class ModelEdge {
+    public UUID source;
+    public UUID target;
+
+    public UUID source() {
+        return source;
+    }
+
+    public UUID target() {
+        return target;
+    }
+
+    public ModelEdge() {
+
+    }
+
+    public ModelEdge(UUID source, UUID target) {
+        this.source = source;
+        this.target = target;
+    }
+}
 
