d6051d8a1207b481f87971b07941c9d7363f36af
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-02 22:01:09 +0100

    wip: add model classes

---
+++ graphedit/src/main/java/dk/gtz/graphedit/syntaxes/ntta/model/ModelEdge.java
@@ -0,0 +1,7 @@
+package dk.gtz.graphedit.syntaxes.ntta.model;
+
+public class ModelEdge extends dk.gtz.graphedit.model.ModelEdge {
+    public String guard;
+    public String update;
+}
+
---
+++ graphedit/src/main/java/dk/gtz/graphedit/syntaxes/ntta/model/ModelLocation.java
@@ -0,0 +1,8 @@
+package dk.gtz.graphedit.syntaxes.ntta.model;
+
+import dk.gtz.graphedit.model.ModelVertex;
+
+public class ModelLocation extends ModelVertex {
+
+}
+
