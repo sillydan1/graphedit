cf3ff5e8bdc6c014269201d49134c4f7860fdf8a
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-07 22:04:44 +0100

    nitpick: just some minor nitpicky newline removals

--- core/src/main/java/dk/gtz/graphedit/util/InspectorUtils.java
+++ core/src/main/java/dk/gtz/graphedit/util/InspectorUtils.java
@@ -285,4 +285,3 @@ public class InspectorUtils {
 	return s;
     }
 }
-
--- core/src/main/java/dk/gtz/graphedit/viewmodel/FileBufferContainer.java
+++ core/src/main/java/dk/gtz/graphedit/viewmodel/FileBufferContainer.java
@@ -78,4 +78,3 @@ public class FileBufferContainer implements IBufferContainer {
         Platform.runLater(() -> openBuffers.put(filename, model));
     }
 }
-
