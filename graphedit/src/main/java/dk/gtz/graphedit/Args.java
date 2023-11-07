26c5231e033980bc6cb04d7ad866695c4ebd1e62
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-07 07:04:06 +0100

    feat: add configdir/plugins as part of the dirs to look for plugins

--- graphedit/src/main/java/dk/gtz/graphedit/Args.java
+++ graphedit/src/main/java/dk/gtz/graphedit/Args.java
@@ -1,8 +1,11 @@
 package dk.gtz.graphedit;
 
+import java.io.File;
 import java.util.List;
 
 import com.beust.jcommander.Parameter;
+
+import dk.gtz.graphedit.util.PlatformUtils;
 
 public class Args {
     @Parameter(names = { "-h", "--help" }, description = "Show this message")
@@ -10,6 +13,15 @@ public class Args {
     @Parameter(names = { "-v", "--verbosity" }, description = "Set verbosity level")
     public String verbosity = "INFO";
     @Parameter(names = { "-P", "--plugin-dir" }, description = "Set directory to look for plugins in")
-    public List<String> pluginDirs = List.of("plugins");
+    public List<String> pluginDirs = List.of("plugins", String.join(File.separator, configDir(), "plugins"));
+
+    private static String configDir() {
+        if(PlatformUtils.isWindows())
+            return String.join(File.separator, System.getenv("AppData"), "graphedit").toString();
+        var userHome = System.getProperty("user.home");
+        if(PlatformUtils.isMac())
+            return String.join(File.separator, userHome, "Library", "Application Support", "Graphedit");
+        return String.join(File.separator, userHome, ".local", "graphedit");
+    }
 }
 
