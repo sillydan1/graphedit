3dbf82c5c7d58801c31a83aed52586058dc187ee
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-07 21:48:59 +0100

    fix: default pref width of the sidepanel
    with the new filepanel "new file" icon button, 300 is _just_ a bit too small

--- core/src/main/java/dk/gtz/graphedit/view/EditorController.java
+++ core/src/main/java/dk/gtz/graphedit/view/EditorController.java
@@ -51,7 +51,7 @@ public class EditorController {
 	selectedRunTarget = Optional.empty();
 	runTargetThread = new Thread(this::runTarget);
 	WidthDragResizer.makeResizableRight((Region)primaryBorderPane.getLeft());
-	((Region)primaryBorderPane.getLeft()).setPrefWidth(300);
+	((Region)primaryBorderPane.getLeft()).setPrefWidth(320);
 	HeightDragResizer.makeResizableUp((Region)primaryBorderPane.getBottom());
 	initProjectMenu();
 	hideTopbarOnSupportedPlatforms();
