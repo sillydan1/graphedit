ec9237219a3416ddbc35afbc43d3aca756fb77f3
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-09 07:25:00 +0100

    docs: add javadoc to model point

--- core/src/main/java/dk/gtz/graphedit/model/ModelPoint.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelPoint.java
@@ -2,6 +2,8 @@ package dk.gtz.graphedit.model;
 
 /**
  * A simple 2D point
+ * @param x The x value of the point
+ * @param y The y value of the point
  */
 public record ModelPoint(double x, double y) {}
 
