4c616c7c350ba01eaf5424c87f8c1663ccad8e6b
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-09 07:24:52 +0100

    docs: add javadoc to model editorsettings

--- core/src/main/java/dk/gtz/graphedit/model/ModelEditorSettings.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelEditorSettings.java
@@ -11,17 +11,41 @@ import dk.gtz.graphedit.viewmodel.ViewModelEditorSettings;
 /**
  * General editor settings model object containing a users' prefered theme, recent projects and other edior-wide preferences and settings.
  * This is meant to be serialized and deserialized to/from disk
+ * @param gridSizeX Width of snapgrid cells
+ * @param gridSizeY Height of snapgrid cells
+ * @param gridSnap When true, vertices will snap to the grid
+ * @param useLightTheme When true, the editor will use a light color scheme
+ * @param autoOpenLastProject When true, the last opened project will be automatically opened next time you start the editor
+ * @param showInspectorPane (Deprecated) doesn't do anything anymore
+ * @param showInfoToasts When true, will display toasts on logger.info calls
+ * @param showWarnToasts When true, will display toasts on logger.warn calls
+ * @param showErrorToasts When true, will display toasts on logger.error calls
+ * @param lastOpenedProject Filepath to the last opened graphedit project file
+ * @param recentProjects List of filepaths that have been recently opened
  */
-public record ModelEditorSettings(double gridSizeX, double gridSizeY, boolean gridSnap, boolean useLightTheme, boolean autoOpenLastProject, @Deprecated boolean showInspectorPane, boolean showInfoToasts, boolean showWarnToasts, boolean showErrorToasts, boolean showTraceToasts, String lastOpenedProject, List<String> recentProjects) {
+public record ModelEditorSettings(
+        double gridSizeX,
+        double gridSizeY,
+        boolean gridSnap,
+        boolean useLightTheme,
+        boolean autoOpenLastProject,
+        @Deprecated boolean showInspectorPane,
+        boolean showInfoToasts,
+        boolean showWarnToasts,
+        boolean showErrorToasts,
+        boolean showTraceToasts,
+        String lastOpenedProject,
+        List<String> recentProjects) {
+
     /**
-     * Creates a ModelEditorSettings instance with default values.
+     * Constructs a ModelEditorSettings instance with default values.
      */
     public ModelEditorSettings() {
         this(20.0d, 20.0d, true, false, true, false, true, true, true, false, "", new ArrayList<>());
     }
 
     /**
-     * Creates a ModelEditorSettings instance based on the associated ViewModel
+     * Constructs a ModelEditorSettings instance based on the associated ViewModel
      * @param viewmodel The viewmodel to base the new instance value off of
      */
     public ModelEditorSettings(ViewModelEditorSettings viewmodel) {
@@ -55,4 +79,3 @@ public record ModelEditorSettings(double gridSizeX, double gridSizeY, boolean gr
         return Path.of(userHome + "graphedit-settings.json");
     }
 }
-
