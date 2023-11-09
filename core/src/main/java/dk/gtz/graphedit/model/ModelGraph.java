c722b0a5fcb8f5bf434fd3a9bdd19876954346a5
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-09 07:27:27 +0100

    docs: add javadoc to model graph

--- core/src/main/java/dk/gtz/graphedit/model/ModelGraph.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelGraph.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 
 /**
  * Container for all syntactic elements in a graph.
+ * @param declarations A string that can contain extraneous textual syntax such as variable declarations, readme data, value ranges, functions etc.
+ * @param vertices A mapping of vertex ids to model vertex-values
+ * @param edges A mapping of edge ids to model edge-values
  */
 public record ModelGraph(
                 String declarations,
