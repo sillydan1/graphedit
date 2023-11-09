d9c44ada189928ac2b37395ad74bf87d6528187f
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-09 07:34:57 +0100

    docs: add javadoc for vertex mouseevent

--- core/src/main/java/dk/gtz/graphedit/events/VertexMouseEvent.java
+++ core/src/main/java/dk/gtz/graphedit/events/VertexMouseEvent.java
@@ -11,6 +11,13 @@ import javafx.scene.transform.Affine;
 
 /**
  * When a {@link MouseEvent} occurs on an {@link ViewModelVertex}.
+ * @param event The javafx {@link MouseEvent} that ocured
+ * @param vertexId The id of the vertex that was interacted with
+ * @param vertex The vertex that was interacted with
+ * @param viewportAffine The {@link Affine} that controls where the viewport is looking
+ * @param syntax The syntax factory associated with the current model
+ * @param graph The current model graph
+ * @param editorSettings The current editor settings
  */
 public record VertexMouseEvent(
 	MouseEvent event,
