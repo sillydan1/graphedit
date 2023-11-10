5d8b08aaf2ced102c90d3b1cf0c91e60dedc6ef1
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 07:32:17 +0100

    docs: add javadoc for isyntaxmigrater

--- core/src/main/java/dk/gtz/graphedit/spi/ISyntaxMigrater.java
+++ core/src/main/java/dk/gtz/graphedit/spi/ISyntaxMigrater.java
@@ -3,6 +3,11 @@ package dk.gtz.graphedit.spi;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+/**
+ * Interface for syntax migraters.
+ * This is useful for syntax plugins whenever they change the model format in a breaking manner.
+ * Migraters can then migrate from an old version to a newer one.
+ */
 @FunctionalInterface
 public interface ISyntaxMigrater {
     // TODO: This is 100% dependent on Jackson serialization... It's technically the best serializer for java, but it's still a hard coupling, which is bad.
