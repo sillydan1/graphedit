02a6ad886a881c196405c675c19410f4a932d038
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 22:10:54 +0100

    docs: add javadoc for model runtarget

--- core/src/main/java/dk/gtz/graphedit/model/ModelRunTarget.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelRunTarget.java
@@ -3,6 +3,16 @@ package dk.gtz.graphedit.model;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Runtarget datastructure. A runtarget is an abstraction over a command-line program call
+ * @param name The name of the runtarget
+ * @param command The command to run - must be a valid path to an executable
+ * @param arguments List of command arguments
+ * @param cwd Where to execute the command from
+ * @param runAsShell When true, will run the command in a shell
+ * @param saveBeforeRun When true, will save the project before running the runtarget
+ * @param environment Mapping of environment variable assignments
+ */
 public record ModelRunTarget(
     String name,
     String command,
