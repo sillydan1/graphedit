b30a24d38f71653d06ab09ad20798747f40754c9
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 22:31:58 +0100

    docs: add javadoc for serializationexceptio

--- core/src/main/java/dk/gtz/graphedit/exceptions/SerializationException.java
+++ core/src/main/java/dk/gtz/graphedit/exceptions/SerializationException.java
@@ -4,16 +4,28 @@ package dk.gtz.graphedit.exceptions;
  * An error occurred during serialization of something
  */
 public class SerializationException extends RuntimeException {
+    /**
+     * Construct a new instance
+     * @param message The detail message
+     */
     public SerializationException(String message) {
         super(message);
     }
 
+    /**
+     * Construct a new instance
+     * @param message The detail message
+     * @param inner The throwable that caused this
+     */
     public SerializationException(String message, Throwable inner) {
         super(message, inner);
     }
 
+    /**
+     * Construct a new instance
+     * @param inner The throwable that caused this
+     */
     public SerializationException(Throwable inner) {
         super(inner);
     }
 }
-
