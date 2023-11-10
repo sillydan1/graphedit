c03d6f2d1e2fd90a2e9f9661dafb924357fe7176
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 22:32:06 +0100

    docs: add javadoc for select tool

--- core/src/main/java/dk/gtz/graphedit/tool/SelectTool.java
+++ core/src/main/java/dk/gtz/graphedit/tool/SelectTool.java
@@ -21,10 +21,19 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
+/**
+ * Tool to select syntactic elements.
+ *
+ * When selected, click either a vertex or an edge to mark it as selected.
+ * You can select more than one element at a time by holding down <CTRL> whilst clicking on a syntactic element.
+ */
 public class SelectTool extends AbstractBaseTool {
     private final Logger logger = LoggerFactory.getLogger(SelectTool.class);
     private final ObservableList<ViewModelSelection> selectedElements;
 
+    /**
+     * Construct a new instance
+     */
     public SelectTool() {
         selectedElements = DI.get("selectedElements");
     }
@@ -83,12 +92,20 @@ public class SelectTool extends AbstractBaseTool {
         }
     }
 
+    /**
+     * Clear the selection
+     */
     public void clear() {
         for(var e : selectedElements) 
             e.selectable().deselect();
         selectedElements.clear();
     }
 
+    /**
+     * Toggle the selection of a syntactic element
+     * @param id The id of the element to select / deselect
+     * @param selectable The selectable object to select / deselect
+     */
     public void toggleSelected(UUID id, ISelectable selectable) {
         if (selectedElements.stream().anyMatch(e -> e.id().equals(id) && e.selectable() == selectable)) {
             selectedElements.removeIf(e -> e.id().equals(id) && e.selectable() == selectable);
@@ -99,8 +116,11 @@ public class SelectTool extends AbstractBaseTool {
         }
     }
 
+    /**
+     * Get the current selection
+     * @return An observable list of selections
+     */
     public ObservableList<ViewModelSelection> getSelection() {
         return selectedElements;
     }
 }
-
