93fb658efb3ac717fd75c7ae936daed1919aadb1
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 22:31:28 +0100

    docs: add javadoc for project load exception

--- core/src/main/java/dk/gtz/graphedit/exceptions/ProjectLoadException.java
+++ core/src/main/java/dk/gtz/graphedit/exceptions/ProjectLoadException.java
@@ -4,8 +4,11 @@ package dk.gtz.graphedit.exceptions;
  * An error occurred during loading a graphedit project
  */
 public class ProjectLoadException extends RuntimeException {
+    /**
+     * Construct a new instance with a message
+     * @param message The detail message
+     */
     public ProjectLoadException(String message) {
         super(message);
     }
 }
-
