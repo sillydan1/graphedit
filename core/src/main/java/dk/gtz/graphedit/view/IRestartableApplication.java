63172026ca94a94d9a5d7ab1b059ecd27f3a46a1
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 20:16:55 +0100

    docs: add javadoc for irestartable application

--- core/src/main/java/dk/gtz/graphedit/view/IRestartableApplication.java
+++ core/src/main/java/dk/gtz/graphedit/view/IRestartableApplication.java
@@ -4,6 +4,8 @@ package dk.gtz.graphedit.view;
  * An interface for applications that are restartable.
  */
 public interface IRestartableApplication {
+    /**
+     * Restart the application
+     */
     void restart();
 }
-
