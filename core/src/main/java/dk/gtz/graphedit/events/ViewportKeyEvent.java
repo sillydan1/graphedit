945f9a14eef303e2a1ba020c43dbdde847a5d6a0
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 06:46:52 +0100

    docs: add javadoc for itool

--- core/src/main/java/dk/gtz/graphedit/events/ViewportKeyEvent.java
+++ core/src/main/java/dk/gtz/graphedit/events/ViewportKeyEvent.java
@@ -10,7 +10,7 @@ import javafx.scene.transform.Affine;
  * When a {@link KeyEvent} occurs on the model editor viewport
  * @param event The javafx {@link KeyEvent} that ocured
  * @param viewportAffine The {@link Affine} that controls where the viewport is looking
- * @param isTargetDrawPane If true, then this event is targeting the drawpane. Useful for filtering unwanted events
+ * @param isTargetDrawpane If true, then this event is targeting the drawpane. Useful for filtering unwanted events
  * @param syntax The syntax factory associated with the current model
  * @param graph The current model graph
  * @param editorSettings The current editor settings
