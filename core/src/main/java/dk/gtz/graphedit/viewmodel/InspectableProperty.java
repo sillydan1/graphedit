559126df838dacd37b2e4a3cfc46f838afe510ae
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 20:02:38 +0100

    docs: add javadoc for inspectable property record

--- core/src/main/java/dk/gtz/graphedit/viewmodel/InspectableProperty.java
+++ core/src/main/java/dk/gtz/graphedit/viewmodel/InspectableProperty.java
@@ -2,5 +2,10 @@ package dk.gtz.graphedit.viewmodel;
 
 import javafx.beans.Observable;
 
+/**
+ * Representation of an {@link Observable} with a {@link String} name
+ * @param name The name of the inspectable property
+ * @param property The property of the inspectable property
+ */
 public record InspectableProperty(String name, Observable property) {}
 
