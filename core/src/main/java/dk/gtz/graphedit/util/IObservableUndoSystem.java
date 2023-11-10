08efa7ab9b235dc2a9b07b0e05a461bf61f92498
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-10 20:11:42 +0100

    docs: add javadoc for iobservable undosystem

--- core/src/main/java/dk/gtz/graphedit/util/IObservableUndoSystem.java
+++ core/src/main/java/dk/gtz/graphedit/util/IObservableUndoSystem.java
@@ -4,7 +4,13 @@ import dk.yalibs.yaundo.IUndoSystem;
 import dk.yalibs.yaundo.Undoable;
 import javafx.beans.value.ObservableValue;
 
+/**
+ * Interface for an undo system where the {@link Undoable} entries are observable
+ */
 public interface IObservableUndoSystem extends IUndoSystem {
+    /**
+     * Get the current {@link Undoable} observable value
+     * @return The current undoable
+     */
     ObservableValue<Undoable> getCurrentUndoableProperty();
 }
-
