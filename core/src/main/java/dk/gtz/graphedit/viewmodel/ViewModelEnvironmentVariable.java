f795269d516b37998227c6f5bb0ad92a58d3ff76
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-11-11 08:10:39 +0100

    docs: add javadoc for viewmodel environment variable

--- core/src/main/java/dk/gtz/graphedit/viewmodel/ViewModelEnvironmentVariable.java
+++ core/src/main/java/dk/gtz/graphedit/viewmodel/ViewModelEnvironmentVariable.java
@@ -2,5 +2,10 @@ package dk.gtz.graphedit.viewmodel;
 
 import javafx.beans.property.StringProperty;
 
+/**
+ * Viewmodel representation of an environment variable and the assigned value of it.
+ * @param key The environment variable name
+ * @param value The value for the environment variable
+ */
 public record ViewModelEnvironmentVariable(StringProperty key, StringProperty value) {}
 
