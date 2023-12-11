89a6439f31c40d77f7b46e9c52a874780363f3f7
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-12-11 15:41:20 +0100

    docs: add javadoc to graphedit.model package

--- core/src/main/java/dk/gtz/graphedit/model/ModelLintSeverity.java
+++ core/src/main/java/dk/gtz/graphedit/model/ModelLintSeverity.java
@@ -1,8 +1,26 @@
 package dk.gtz.graphedit.model;
 
+/**
+ * Severity level enumeration for lints.
+ */
 public enum ModelLintSeverity {
+    /**
+     * Hint-level lints are meant for small notifications about things that could be optimized.
+     */
     HINT,
+
+    /**
+     * Info-level lints are meant to indicate general non-problematic information.
+     */
     INFO,
+
+    /**
+     * Warning-level lints are meant to indicate if something can potentially go wrong or if something is non-critically erronious.
+     */
     WARNING,
+
+    /**
+     * Error-level lints are meant to indicate when something is erronious and should be fixed.
+     */
     ERROR
 }
--- core/src/main/java/dk/gtz/graphedit/model/lsp/ModelLanguageServerProgressType.java
+++ core/src/main/java/dk/gtz/graphedit/model/lsp/ModelLanguageServerProgressType.java
@@ -1,7 +1,26 @@
 package dk.gtz.graphedit.model.lsp;
 
+/**
+ * Enumeration of the type of language server progress reports.
+ */
 public enum ModelLanguageServerProgressType {
+        /**
+         * Indicates that a language server process has started.
+         */
         BEGIN,
+
+        /**
+         * Indicates that a language server process has progressed, but is still running.
+         */
         PROGRESS,
-        END
+
+        /**
+         * Indiccates that a language server process has finished successfully.
+         */
+        END,
+
+        /**
+         * Indicates that a language server process has finished errorniously.
+         */
+        END_FAIL
 }
