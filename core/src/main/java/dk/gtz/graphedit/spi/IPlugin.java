2e91b09d5b9f6024a91ce71a76affaa167c6d6a5
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2023-12-11 15:41:33 +0100

    docs: add missing javadocs to graphedit.spi package

--- core/src/main/java/dk/gtz/graphedit/spi/IPlugin.java
+++ core/src/main/java/dk/gtz/graphedit/spi/IPlugin.java
@@ -59,6 +59,11 @@ public interface IPlugin {
         return List.of();
     }
 
+    /**
+     * Get a collection of plugin language servers provided by this plugin. Will return an empty list by default.
+     * @return A collection of language servers
+     * @throws Exception Allowed to throw any kind of exception. See the specific plugin implementation for details
+     */
     default Collection<ILanguageServer> getLanguageServers() throws Exception {
         return List.of();
     }
