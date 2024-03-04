4e8a53c4d8b8271639f7a38744336e43ec84991c
Asger Gitz-Johansen <asger.gitz@hotmail.com>
2024-03-04 06:44:24 +0100

    feat: add autoproperty abstract base class to help with boilerplate
    This is not breaking, as you can still simply override the functions

---
+++ core/src/main/java/dk/gtz/graphedit/viewmodel/Autolisten.java
@@ -0,0 +1,7 @@
+package dk.gtz.graphedit.viewmodel;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Autolisten {}
--- std/src/main/java/dk/gtz/graphedit/plugins/syntaxes/petrinet/viewmodel/ViewModelArc.java
+++ std/src/main/java/dk/gtz/graphedit/plugins/syntaxes/petrinet/viewmodel/ViewModelArc.java
@@ -9,20 +9,18 @@ import org.slf4j.LoggerFactory;
 
 import dk.gtz.graphedit.model.ModelEdge;
 import dk.gtz.graphedit.plugins.syntaxes.petrinet.model.ModelArc;
+import dk.gtz.graphedit.viewmodel.Autolisten;
 import dk.gtz.graphedit.viewmodel.ISearchable;
 import dk.gtz.graphedit.viewmodel.InspectableProperty;
 import dk.gtz.graphedit.viewmodel.ViewModelEdge;
 import dk.gtz.graphedit.viewmodel.ViewModelGraph;
-import javafx.beans.InvalidationListener;
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 
 public class ViewModelArc extends ViewModelEdge implements ISearchable {
 	private static final Logger logger = LoggerFactory.getLogger(ViewModelArc.class);
-	private final IntegerProperty weight;
+	@Autolisten
+	public final IntegerProperty weight;
 
 	public ViewModelArc(UUID uuid, ModelEdge edge) {
 		super(uuid, edge);
@@ -74,69 +72,6 @@ public class ViewModelArc extends ViewModelEdge implements ISearchable {
 	@Override
 	public List<String> getSearchValues() {
 		return List.of("weight: " + weight.get());
-	}
-
-	@Override
-	public void addListener(ChangeListener<? super ViewModelEdge> listener) {
-		super.addListener(listener);
-		weight.addListener((e,o,n) -> listener.changed(this,this,this));
-	}
-
-	@Override
-	public void removeListener(ChangeListener<? super ViewModelEdge> listener) {
-		super.addListener(listener);
-		weight.removeListener((e,o,n) -> listener.changed(this,this,this));
-	}
-
-	@Override
-	public void addListener(InvalidationListener listener) {
-		super.addListener(listener);
-		weight.addListener(listener);
-	}
-
-	@Override
-	public void removeListener(InvalidationListener listener) {
-		super.addListener(listener);
-		weight.removeListener(listener);
-	}
-
-	@Override
-	public void setValue(ViewModelEdge value) {
-		super.setValue(value);
-		if(value instanceof ViewModelArc tvalue)
-			weight.setValue(tvalue.weight().get());
-	}
-
-	@Override
-	public void bind(ObservableValue<? extends ViewModelEdge> observable) {
-		super.bind(observable);
-		if(observable.getValue() instanceof ViewModelArc tobs)
-			weight.bind(tobs.weight());
-	}
-
-	@Override
-	public void unbind() {
-		super.unbind();
-		weight.unbind();
-	}
-
-	@Override
-	public boolean isBound() {
-		return super.isBound() && weight.isBound();
-	}
-
-	@Override
-	public void bindBidirectional(Property<ViewModelEdge> other) {
-		super.bindBidirectional(other);
-		if(other.getValue() instanceof ViewModelArc tother)
-			weight.bindBidirectional(tother.weight());
-	}
-
-	@Override
-	public void unbindBidirectional(Property<ViewModelEdge> other) {
-		super.unbindBidirectional(other);
-		if(other.getValue() instanceof ViewModelArc tother)
-			weight.unbindBidirectional(tother.weight());
 	}
 
 	@Override
